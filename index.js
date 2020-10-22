7e527aed895eb33647550816e85db11b38a90d2f
Erico da Silva Bruner <erico.bruner@redeunifique.com.br>
2020-10-22 19:16:40 -0300

    atualizando...

---
+++ index.js
@@ -0,0 +1,9 @@
+const {Builder, By, Key, until} = require('selenium-webdriver');
+
+async function example() {
+    let driver = await new Builder().forBrowser('firefox').build();
+    await driver.get('http://191.253.103.90/');
+    await driver.findElement(By.name('Username')).sendKeys('admin', Key.RETURN)
+    await driver.findElement(By.name('Password')).sendKeys('glock9mm', Key.RETURN)
+    }
+example()
