1f957258b731684c6e380b9b6bd54e55ffaa46af
Erico Bruner <ericodasilvabruner@gmail.com>
2020-10-23 14:17:55 -0300

    atualizando...

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const {Builder, By, Key, until} = require('selenium-webdriver');
 
+IP = "191.253.103.90"
+PON = "getpage.gch?pid=1002&nextpage=pon_status_link_info_t.gch"
+
 async function example() {
-    let driver = await new Builder().forBrowser('firefox').build();
-    await driver.get('http://191.253.103.90/');
-    await driver.findElement(By.name('Username')).sendKeys('admin', Key.RETURN)
-    await driver.findElement(By.name('Password')).sendKeys('glock9mm', Key.RETURN)
-    await driver.findElement(By.className('menuPlusSymbol')).click()
+    let driver = await new Builder().forBrowser('chrome').build();
+
+    await driver.get(`http://${IP}/`)
+
+    await driver.findElement(By.name('Username')).sendKeys('')
+    await driver.findElement(By.name('Password')).sendKeys('')
+
+    await driver.findElement(By.id('LoginId')).click()
+
+    await driver.get(`http://${IP}/`+PON)
+    
+
     }
+
 example()
