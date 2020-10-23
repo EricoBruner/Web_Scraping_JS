072ed937867e2904d62c3684d1559f5be0e27865
Erico Bruner <ericodasilvabruner@gmail.com>
2020-10-23 20:45:21 -0300

    atualizando...

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ IP = "191.253.103.90"
 PON = "getpage.gch?pid=1002&nextpage=pon_status_link_info_t.gch"
 
 async function example() {
-    let driver = await new Builder().forBrowser('chrome').build();
+    let driver = await new Builder(headless = false).forBrowser('chrome').build();
+    driver.manage().window().minimize();
 
     await driver.get(`http://${IP}/`)
 
@@ -16,6 +17,16 @@ async function example() {
     await driver.findElement(By.id('LoginId')).click()
 
     await driver.get(`http://${IP}/`+PON)
+    pon_status = await driver.findElement(By.id("Fnt_RxPower")).getText()
 
+    if (pon_status > -28) {
+        console.log("=========================")
+        console.log("Sinal dentro do PDQ "+pon_status)
+        console.log("=========================")
+    }else{
+        console.log("=========================")
+        console.log("Sinal fora do PDQ "+pon_status)
+        console.log("=========================")
     }
+}
 example()
